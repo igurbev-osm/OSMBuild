35cd883636158118d8bc202ac68344c116b94bdc
igurbev-osm <igurbev@gmail.com>
2016-11-25 12:53:02 +0200

    src

---
+++ build.bat
@@ -0,0 +1,24 @@
+for /f "delims=" %%x in (%1) do (set "%%x")
+
+wget -O download/%download_name% %download_url%
+
+del /F /S /Q %output_dir%
+mkdir %output_dir%
+cd %output_dir%
+
+copy ..\conf\next.args .
+sed -i "s/{family_id}/%family_id%/g" next.args
+sed -i "s/{instalation_name}/%instalation_name%/g" next.args
+sed -i "s/{country-name}/%country-name%/g" next.args
+sed -i "s/{country-abbr}/%country-abbr%/g" next.args
+
+java -jar ../mkgmap-r3701/mkgmap.jar --family-id=%family_id% ../conf/typfile.txt
+::../srtm2osm.exe -bounds1 41.1330000 22.3410000 44.2650000 28.6690000 -cat 400 100 -large -corrxy 0.0005 0.0006 -step 10 -o %srtm_file%
+java -Xmx1500m -XX:MaxHeapSize=1024m -jar ../splitter-r439/splitter.jar --max-nodes=1600000 --max-areas=512 --mapid=%mapid%  --keep-complete=false --description="%description%" --mixed ../Srtm2Osm/%srtm_file% ../download/%download_name%
+java -Xmx1024m -jar ../mkgmap-r3701/mkgmap.jar -c next.args -c template.args %family_id%*.osm.pbf typfile.typ
+
+sed -i "s/OSM map/%instalation_name%/g" osmmap.nsi
+sed -i "s/xtypfile.typ/typfile.typ/g" osmmap.nsi
+makensis osmmap.nsi
+
+cd ..
