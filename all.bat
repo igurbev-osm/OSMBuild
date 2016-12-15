25a2b6b166e6a65cbb8695ae612f2fb4e50660c7
igurbev-osm <igurbev@gmail.com>
2016-12-15 14:35:24 +0200

    Ново пропърти за размер на сплит нодовете

--- all.bat
+++ all.bat
@@ -5,7 +5,7 @@ call build.bat conf\makedonia.props
 call build.bat conf\montenegro.props
 call build.bat conf\slovenia.props
 call build.bat conf\bih.props
-call build.bat conf\kosovo.props
+::call build.bat conf\kosovo.props
 call build.bat conf\croatia.props
 call build.bat conf\serbia.props
 call build.bat conf\romania.props
--- build.bat
+++ build.bat
@@ -26,7 +26,7 @@ sed -i "s/{src_dir}/"%src_dir%"/g" next.args
 java -jar ../mkgmap-r3701/mkgmap.jar --family-id=%family_id% %src_dir%\conf\typfile.txt
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-java -d64 -XX:MaxHeapSize=5000m -jar ../splitter-r439/splitter.jar --max-nodes=6000000 --max-areas=512 --mapid=%mapid%  --keep-complete=false --description="%description%" --mixed %srtm_base%%srtm_file% ../download/%download_name%
+java -d64 -XX:MaxHeapSize=5000m -jar ../splitter-r439/splitter.jar --max-nodes=%max_split_nodes% --max-areas=512 --mapid=%mapid%  --keep-complete=false --description="%description%" --mixed %srtm_base%%srtm_file% ../download/%download_name%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 java -d64 -XX:MaxHeapSize=5000m -XX:-UseGCOverheadLimit -jar ../mkgmap-r3701/mkgmap.jar --style-file=%src_dir%/styles/mystyle -c next.args -c template.args --gmapsupp %family_id%*.osm.pbf typfile.typ
